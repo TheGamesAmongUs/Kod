3e3272491afec145a9ce8a2bc450917a11d1b20c
Maciej Wroński <mcwron1u@gmail.com>
2015-05-26 17:37:09 +0200

    Create footer2.php

---
+++ site/footer2.php
@@ -0,0 +1,38 @@
+<up>
+<a href="#"><img src="img/up.png"></img></a>
+</up>
+<div id="z1">
+TheGamesAmongUs</br>
+
+<a href="#">O nas</a></br>
+<a href="#">Polityka prywatności</a></br>
+<a href="#">Polityka cookies</a></br>
+<a href="#">Kontakt</a></br>
+<a href="#">Regulamin</a>
+</div>
+<div id="z2">
+Mój Profil</br>
+
+<a href="#">Profil</a></br>
+<a href="#">Zapamiętane Gry</a></br>
+<a href="#">Ocenione Gry</a></br>
+<a href="logout.php">Wyloguj Się</a></br>
+</div>
+<div id="z3">
+Kontakt</br>
+
+<a href="#">O nas</a></br>
+<a href="#">Polityka prywatności</a></br>
+<a href="#">Polityka cookies</a></br>
+<a href="#">Kontakt</a></br>
+</div>
+<div id="z4">
+Zapisz Się Do Newslettera</br>
+
+<div id="newsbar" align="right">
+<form action="newsletter.php" method="POST">
+<input type="text" placeholder="Wpisz swój adres email..." class="newslet" name="newslet"></input><br><br>
+<input type="submit" value="Wyślij" class="btn"></input>
+</form>
+</div>
+</div>
