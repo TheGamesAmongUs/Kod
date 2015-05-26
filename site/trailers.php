f6f8b988213c5d87be422a1b80f0e65f30994988
Maciej Wroński <mcwron1u@gmail.com>
2015-05-26 17:38:56 +0200

    Create trailers.php

---
+++ site/trailers.php
@@ -0,0 +1,4 @@
+<div class="trailers" align="center">
+<h4 align="left">Zwiastuny</h4>
+<table><tr></tr><tr><td><a href="#"><img src="img/bf4.jpg"></img></a></td><td><a href="#"><img src="img/cod.jpg"></img></a></td><td> <a href="#"><img src="img/cs.jpg"></img></a></td></tr><tr><td>Battlefield 4</td><td>Call of Duty: Ghost</td><td>Counter Strike GO</td></tr></table>
+</div>
