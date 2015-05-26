de4717519541642afa9edb602259e7c50ca17fec
Maciej Wroński <mcwron1u@gmail.com>
2015-05-26 17:36:35 +0200

    Create bheadl.php

---
+++ site/bheadl.php
@@ -0,0 +1,11 @@
+<?php
+// rozpoczęcie buforowania (jest to potrzebne by nie mieć błędów typu headers already sent)
+ob_start();
+
+// start sesji
+session_start();
+
+require('config.php');
+
+
+?>
