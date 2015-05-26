22bbe5446d2241db2ba04a780ab416b18f6871b1
Maciej Wroński <mcwron1u@gmail.com>
2015-05-26 17:38:31 +0200

    Create menu.php

---
+++ site/menu.php
@@ -0,0 +1,7 @@
+<menu>
+<a href="index.php">Strona główna</ac>
+<?php if($_SESSION['logged']){ echo'<a href="profile.php">Mój Profil</a>';} ?>
+<a href="gamelist.php">Lista Gier</a>
+<a href="about.php">O Nas</a>
+<a href="contact.php">Kontakt</a>
+</menu>
