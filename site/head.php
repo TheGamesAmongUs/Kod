cbaa296c07f4a15029bc871347735f491f77ead4
Maciej Wroński <mcwron1u@gmail.com>
2015-05-26 17:37:43 +0200

    Create head.php

---
+++ site/head.php
@@ -0,0 +1,5 @@
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="theme.css">
+<link rel="shortcut icon" href="favicon.ico" />
+<script src="script.js"></script>
+<script src="script2.js"></script>
