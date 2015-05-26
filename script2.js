3c9623cfcfdaa0e35dc51b69d0dd775f8a037ea3
Maciej Wroński <mcwron1u@gmail.com>
2015-05-26 17:35:20 +0200

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,10 @@
+function temp_form()
+{
+
+var mail_t = document.forms[1].elements[0].value;
+var nick_t = document.forms[1].elements[1].value;
+var haslo_t = document.forms[1].elements[2].value;
+
+}
+
+
