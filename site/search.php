e65b2113f5174627af313ff431c5d2d487444418
Maciej Wroński <mcwron1u@gmail.com>
2015-05-26 17:38:43 +0200

    Create search.php

---
+++ site/search.php
@@ -0,0 +1,9 @@
+<logo>
+<a href="index.php"><img src="img/logo.png"></img></a>
+</logo>
+<search>
+<form action="search.php"  method="POST" >
+<input type="text" placeholder="Wpisz szukaną frazę..." name="q" class="searchbar" ></input>
+<input type="submit" value="Szukaj" class="btn"></input>
+</form>
+</search>
